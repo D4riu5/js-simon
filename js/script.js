ddf0b067fde4a781220e81c0cf026e116db766e8
D4riu5 <R3iji94@gmail.com>
2023-01-15 07:49:45 +0100

    added countdown on page, cleaned up and optimized the code. still need to fix 2 bugs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 // selectors
 const startGame = document.querySelector(".start");
 const tryAgain = document.querySelector(".re-start");
+const timerOnPage = document.getElementById("showTimer");
 const box1 = document.getElementById("box1");
 const box2 = document.getElementById("box2");
 const box3 = document.getElementById("box3");
 const box4 = document.getElementById("box4");
 const box5 = document.getElementById("box5");
+
+// counters 
+let countDown = 30;
 
 // arrays
 const boxes = [box1, box2, box3, box4, box5];
@@ -41,27 +45,48 @@ function simonSays() {
       }
     }, 1);
     tryAgain.disabled = false;  
-  }, 30000);
+  }, 31400);
   
   startGame.style.display = "none";
   tryAgain.style.display = "inline";
 }
 
+function resetBoxes() {
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = "";
+    boxes[i].classList.remove("my-bg-green", "my-bg-red");
+  }
+}
+
+function myCountdown () {
+  const timer = setInterval(function() {
+    if (countDown == 0) {
+      clearInterval(timer);
+      timerOnPage.innerHTML = "";
+      countDown = 30;
+    } else {
+      timerOnPage.innerHTML = `<h2>Timer: ${countDown}</h2>`;
+      countDown--;
+    }
+  },1000)
+}
+
 // events
 startGame.addEventListener("click", function() {
   addNumberToBoxes();
+  myCountdown();
   simonSays();
 
 });
 
-// resetting the game
-  tryAgain.addEventListener("click", function(){
+tryAgain.addEventListener("click", function(){
+  resetBoxes();
+  addNumberToBoxes();
+  myCountdown();
+  simonSays();
+});
 
-    for (let i = 0; i < boxes.length; i++) {
-      boxes[i].innerHTML = "";
-      boxes[i].classList.remove("my-bg-green", "my-bg-red");
-    }
-    addNumberToBoxes();
-    simonSays();
-  });
 
+// BUGS ???
+// 1: if i minimeze the browser tab while im waiting for the timer to finish, the numbers dont disappear from the boxes
+// 2: the countdown has a 1.5 second delay before showing up on screen. cant figure this out so i just increased the timeout from 30 seconds to 31.5 seconds
